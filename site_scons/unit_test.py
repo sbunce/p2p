8747c78fc27c661be74b2a88871e5580fd5172d9
Seth Bunce <seth@seth.dnsdojo.net>
2009-05-08 18:54:11 -0700

    Ignore unit text executibles.

--- site_scons/unit_test.py
+++ site_scons/unit_test.py
@@ -1,14 +1,20 @@
 #std
 import os
+import re
 import sys
 from SCons.Script.SConscript import SConsEnvironment
 
 def unit_test(env, source):
-	test = env.Program(source)
-	if sys.platform == 'linux2':
-		env.AddPostAction(test, 'cd '+os.getcwd()+' && ./'+str(test[0]))
 	if sys.platform == 'win32':
+		test = env.Program(source)
 		env.AddPostAction(test, 'cd '+os.getcwd()+' && '+str(test[0]))
+	else:
+		#give posix systems a file extension that mercurial can ignore
+		name = re.split('\.', source[0])
+		name = name[0] + '.out'
+		test = env.Program(name, source)
+		env.AddPostAction(test, 'cd '+os.getcwd()+' && ./'+str(test[0]))
+
 	return test
 
 SConsEnvironment.unit_test = unit_test
